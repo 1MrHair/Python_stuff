94b558311f7a64e88317f3d1b149f0e551025886
1MrHair <kennethsagui4@gmail.com>
2026-02-04 14:10:05 -0800

    Create built_in_functions.py

---
+++ built_in_functions.py
@@ -0,0 +1,11 @@
+yap = "This uses the print built-in function!"
+print(yap)
+x = input("This will be the value of x > ")
+x = int(x) # makes the x an int
+print(type(True)) # prints that true is a boolean
+print(abs(x)) # if x is negative, then it will become positive
+print(len(yap)) # How many characters the string has
+print(oct(x)) # prints the octal value of x
+print(pow(x, 2)) # raises x to the power of 2
+print(reversed(yap)) # reverses the string
+print(bin(x)) # turns x into binary
