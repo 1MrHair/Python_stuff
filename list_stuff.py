876a1c9a05f13191af6e113d82d6a8ea1e33f4e5
1MrHair <kennethsagui4@gmail.com>
2026-02-09 14:55:23 -0800

    Update list_stuff.py

--- list_stuff.py
+++ list_stuff.py
@@ -10,3 +10,10 @@ print(len(a_list))
 a_list2 = ['2nd','list']
 a_list3 = a_list + a_list2
 print(a_list3)
+a = [1,2,3,4,5,6,7,8,9,0]
+b = ['a','b','c','d','e','f','g']
+c = ['z',0.67,10,True,['Another','List']]
+print(a,b,c)
+print(c[4])
+d = c[4]
+print(d[0])
